db6a63f50ae934fc8558ad7961e57a7c8fc9bca2
Andrey Emelianov <621077@mail.ru>
2023-12-27 17:17:19 +0300

    [feat(category)]: реализовал dto для категорий и реализовал CRUD методы для категорий, проверил через insomnia, всё отрабатывает корректно

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,26 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Repository } from 'typeorm';
+import { Category } from './entities/category.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto, id: number) {
+    const isExist = await this.categoryRepository.findBy({
+      user: { id },
+      title: createCategoryDto.title,
+    });
+
+    if (isExist.length)
+      throw new BadRequestException('Такая категория уже существует');
+
+    const newCategory = {
+      title: createCategoryDto.title,
+      user: {
+        id,
+      },
+    };
+
+    return await this.categoryRepository.save(newCategory);
   }
 
-  findAll() {
-    return `This action returns all category`;
+  async findAll(id: number) {
+    return await this.categoryRepository.find({
+      where: {
+        user: { id },
+      },
+      relations: {
+        transactions: true,
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: number) {
+    const isExistCategory = await this.categoryRepository.findOne({
+      where: { id },
+      relations: {
+        user: true,
+        transactions: true,
+      },
+    });
+
+    if (!isExistCategory) throw new NotFoundException('Категория не найдена');
+
+    return isExistCategory;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    const isExistCategory = await this.categoryRepository.findOne({
+      where: { id },
+    });
+
+    if (!isExistCategory) throw new NotFoundException('Категория не найдена');
+
+    return await this.categoryRepository.update(id, updateCategoryDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: number) {
+    const isExistCategory = await this.categoryRepository.findOne({
+      where: { id },
+    });
+
+    if (!isExistCategory) throw new NotFoundException('Категория не найдена');
+
+    return await this.categoryRepository.delete(id);
   }
 }
